605da1c956632cfd144028aac74b51baf01626c5
Diego <poncelas91@gmail.com>
2021-02-10 16:06:37 +0100

    Add functionality to get most liked photos

--- InstagramApp/main_ins.py
+++ InstagramApp/main_ins.py
@@ -1,5 +1,6 @@
 import instaloader
 import json
+import heapq
 
 
 
@@ -17,35 +18,48 @@ class InstagramApp:
         self._profile = instaloader.Profile.from_username(self._context, user)
 
     def login(self):
+        print(f"Start application for user {self.user}")
         try:
             self._instaloader.login(self.user, self.password)
-            print("Login successful")
+            print("Login successful\n")
         except instaloader.InstaloaderException:
-            print(f"Exception found")
+            print(f"Exception found\n")
             raise
 
     def close(self):
         self._instaloader.close()
 
+    # Return followers
     def followers(self):
         return [f.username for f in self._profile.get_followers()]
 
+    # Return followees
     def followees(self):
         return [f.username for f in self._profile.get_followees()]
 
+    # Return top n likes and posts url
+    def top_n_likes(self, n):
+        print(f"Getting the {n} most liked photos for {self.user}...")
+        # Get post likes and shortcodes
+        dict_likes = {f.shortcode: f.likes for f in self._profile.get_posts()}
+        # Get max number of likes
+        lst = heapq.nlargest(n, [value for key, value in dict_likes.items()])
+        # Get results
+        sorted_dict = sorted(dict_likes.items(), key=lambda item: item[1], reverse=True)
+        for element in [(key, value) for key, value in sorted_dict if value in lst]:
+            print(f"Post url: https://www.instagram.com/p/{element[0]}/    Likes: {element[1]}")
+
+    # Get people that doesn't follow you back
     def unfollow(self):
-        print(f"Checking people that don't follow {self.user} back...")
-        skip_list = ["pycoders", "wildlifeplanet", "earth", "heroefitness", "gastroobscura", "natgeoadventure",
-                     "spacex", "danielkordan", "natgeo", "kpunkka", "davidlloyd", "itsabandoned", "8fact", "science",
-                     "papateachme", "scoobysworkshop", "bbcnews", "ancient_origins", "atlasobscura",
-                     "jimmy.nelson.official", "atodaleche", "teslamotors", "cuestafrank", "natgeotravel", "paulnicklen",
-                     "madrid_secreto", "iflscience", "nasa", "eminem", "discoverwildlife", "jmvillatoro_psicologo",
-                     "ancient.scientist"]
-        followers = user1.followers()
-        followees = user1.followees()
+        print(f"Checking followees that aren't followers...")
+        skip_list = [i.rstrip("\n") for i in open("skip_list.txt", "r")]
+        followers = self.followers()
+        followees = self.followees()
         for f in followees:
             if f not in followers and f not in skip_list:
                 print(f)
+        print()
+
 
 
 
@@ -55,4 +69,5 @@ if __name__ == '__main__':
     user1 = InstagramApp(user_cred["user"], user_cred["password"])
     user1.login()
     user1.unfollow()
+    user1.top_n_likes(10)
     user1.close()
